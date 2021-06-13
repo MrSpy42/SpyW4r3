c3d04f8bed993ecdacaa76b2f9fbf6ec55451b54
MrSpy42 <nikiforos007gr@gmail.com>
2021-06-13 11:11:50 +0300

    first commit

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace SpyW4r3
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.RichTextBox();
+            this.sendBox = new System.Windows.Forms.TextBox();
+            this.sendText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ipSend = new System.Windows.Forms.TextBox();
+            this.portSend = new System.Windows.Forms.TextBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.requestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Location = new System.Drawing.Point(138, 10);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ReadOnly = true;
+            this.mainTextBox.Size = new System.Drawing.Size(650, 373);
+            this.mainTextBox.TabIndex = 0;
+            this.mainTextBox.Text = "";
+            // 
+            // sendBox
+            // 
+            this.sendBox.Location = new System.Drawing.Point(183, 404);
+            this.sendBox.Name = "sendBox";
+            this.sendBox.Size = new System.Drawing.Size(452, 23);
+            this.sendBox.TabIndex = 1;
+            // 
+            // sendText
+            // 
+            this.sendText.AutoSize = true;
+            this.sendText.Location = new System.Drawing.Point(138, 407);
+            this.sendText.Name = "sendText";
+            this.sendText.Size = new System.Drawing.Size(39, 15);
+            this.sendText.TabIndex = 2;
+            this.sendText.Text = "Send :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(12, 46);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(86, 23);
+            this.portTextBox.TabIndex = 6;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(12, 75);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(118, 23);
+            this.connectButton.TabIndex = 7;
+            this.connectButton.Text = "Start Listener";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(649, 403);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 8;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "IP Send Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Send Port";
+            // 
+            // ipSend
+            // 
+            this.ipSend.Location = new System.Drawing.Point(13, 167);
+            this.ipSend.Name = "ipSend";
+            this.ipSend.Size = new System.Drawing.Size(100, 23);
+            this.ipSend.TabIndex = 11;
+            // 
+            // portSend
+            // 
+            this.portSend.Location = new System.Drawing.Point(13, 225);
+            this.portSend.Name = "portSend";
+            this.portSend.Size = new System.Drawing.Size(100, 23);
+            this.portSend.TabIndex = 12;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(13, 345);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(100, 23);
+            this.usernameBox.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 324);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Username";
+            // 
+            // requestButton
+            // 
+            this.requestButton.Location = new System.Drawing.Point(12, 255);
+            this.requestButton.Name = "requestButton";
+            this.requestButton.Size = new System.Drawing.Size(75, 23);
+            this.requestButton.TabIndex = 16;
+            this.requestButton.Text = "Connect";
+            this.requestButton.UseVisualStyleBackColor = true;
+            this.requestButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.requestButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.portSend);
+            this.Controls.Add(this.ipSend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sendText);
+            this.Controls.Add(this.sendBox);
+            this.Controls.Add(this.mainTextBox);
+            this.Name = "FrmMain";
+            this.Text = "SpyW4r3 - Messaging";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox mainTextBox;
+        private System.Windows.Forms.Label sendText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox sendBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ipSend;
+        private System.Windows.Forms.TextBox portSend;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button requestButton;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SpyW4r3
+{
+    public partial class FrmMain : Form
+    {
+        static int port;
+        static IPAddress ip;
+        static int portS = -1;
+        static IPAddress ipS;
+        static messageClient server;
+        static Thread t;
+        static Thread sendThread;
+        static string username;
+        static bool sendBool = true;
+        static bool receiveBool = true;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.FormClosing += new FormClosingEventHandler(FrmMain_FormClosing);
+            
+        }
+
+        private void FrmMain_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            //cancels everything if threads are still alive.
+            
+            if (!(t == null) || !(sendThread == null))
+            {
+                if (t.IsAlive || sendThread.IsAlive)
+                {
+                    sendBool = false;
+                    receiveBool = false;
+                }
+            }
+        }
+
+        //Setup connections/start up all threads.
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            mainTextBox.AppendText("Opening listener...");
+            try { 
+                ip = IPAddress.Parse("127.0.0.1");
+                port = Int32.Parse(portTextBox.Text);
+                server = new messageClient(ip, port);
+                sendThread = new Thread(sendHandler);
+                sendThread.Start();
+                t = new Thread(handler);
+                t.Start();
+
+                this.portTextBox.ReadOnly = true;
+                this.connectButton.Enabled = false;
+                mainTextBox.AppendText(" Connected\n");
+            } catch(Exception exc) {
+                mainTextBox.AppendText("Failed : "+ exc.Message + "\n");
+            }
+            
+        }
+
+        //
+        //  INCOMING CONNECTION THREAD
+        //
+        public void handler()
+        {
+            byte[] d;
+            MethodInvoker mi;
+            while (receiveBool)
+            {
+                if(server.listener.Pending())
+                {
+                    TcpClient client = server.listener.AcceptTcpClient();
+                    NetworkStream s = client.GetStream();
+
+                    d = server.receive(s);
+
+                    if (Encoding.ASCII.GetString(d).Contains("!requestConnect"))
+                    {
+                        StringBuilder str = new StringBuilder();
+                        StringBuilder str2 = new StringBuilder();
+                        str2.Append(Encoding.ASCII.GetString(d));
+                        str.Append(client.Client.LocalEndPoint.ToString());
+                        server.encryptionKey = server.tempPort;
+                        server.keyPort = server.tempPort;
+                        string[] Ip = str.ToString().Split(":");
+                        string[] Port = str2.ToString().Split(",");
+                        ipS = IPAddress.Parse(Ip[0]);
+                        portS = Int32.Parse(Port[1]);
+                        mi = delegate ()
+                        {
+                            this.mainTextBox.AppendText("Established connection with client. \n");
+                            this.portSend.Text = Port[1];
+                            this.ipSend.Text = Ip[0];
+                            this.portSend.Enabled = false;
+                            this.ipSend.Enabled = false;
+                            this.requestButton.Enabled = false;
+                        };
+                        this.Invoke(mi);
+                    }
+
+                    d = Encoding.ASCII.GetBytes(server.DecryptString(d, server.encryptionKey, server.IV));
+
+                    mi = delegate () {
+                        this.mainTextBox.AppendText(Encoding.ASCII.GetString(d));
+                        this.mainTextBox.AppendText("\n");
+                    };
+                    this.Invoke(mi);
+                    client.Close();
+                    Thread.Sleep(15);
+                }
+            }
+            Thread.Sleep(15);
+        }
+        //
+        // SEND THREAD
+        //
+        public void sendHandler()
+        {
+            while(sendBool)
+            {
+                if(!(server == null))
+                {
+                    if (server.sendQueue != null)
+                    {
+                        TcpClient client = new TcpClient(ipS.ToString(), portS);
+                        NetworkStream str = client.GetStream();
+                        byte[] msg = { 0xFE };
+                        if (Encoding.ASCII.GetString(server.sendQueue).Contains("!requestConnect"))
+                        {
+                            msg = server.sendQueue;
+                        } else {
+                            msg = server.sendQueue;
+                            MethodInvoker mi = delegate () { this.mainTextBox.AppendText("YOU: " + this.sendBox.Text + "\n"); };
+                            this.Invoke(mi);
+                        }
+                        str.Write(msg, 0, msg.Length);
+
+                        server.sendQueue = null;
+                        client.Close();
+                    }
+                }
+                Thread.Sleep(15);
+            }
+        }
+        //sends message ( sets port/ip send and username )
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            portS = Int32.Parse(this.portSend.Text);
+            ipS = IPAddress.Parse(this.ipSend.Text);
+            username = this.usernameBox.Text;
+
+            String s = username + " : " + this.sendBox.Text;
+            server.send(server.EncryptString(s, server.encryptionKey, server.IV));
+        }
+
+        //REQUEST CONNECT WITH ALREADY LISTENING CLIENT
+        private void button1_Click(object sender, EventArgs e)
+        {
+            portS = Int32.Parse(this.portSend.Text);
+            ipS = IPAddress.Parse(this.ipSend.Text);
+
+            this.portSend.Enabled = false;
+            this.ipSend.Enabled = false;
+            this.requestButton.Enabled = false;
+
+            server.encryptionKey = Encoding.ASCII.GetBytes(portS.ToString() + "0an003vb0!0e");
+            server.keyPort = Encoding.ASCII.GetBytes(portS.ToString() + "0an003vb0!0e");
+
+            server.send(Encoding.ASCII.GetBytes("!requestConnect," + port.ToString()));
+            Thread.Sleep(50);
+        }
+    }
+
+    //
+    // MESSAGE CLIENT CLASS
+    //
+    public class messageClient
+    {
+        public byte[] sendQueue = null;
+        public TcpListener listener;
+        public byte[] keyPort = new byte[16];
+        public readonly byte[] tempPort = new byte[16];
+        public byte[] encryptionKey = new byte[16];
+        public byte[] IV = Encoding.ASCII.GetBytes("spywarebestware1");
+        
+        public messageClient(IPAddress ip,int port)
+        {
+            listener = new TcpListener(ip, port);
+            listener.Start();
+            tempPort = Encoding.ASCII.GetBytes(port.ToString() + "000000000000");
+        }
+        public byte[] receive(NetworkStream stream)
+        {
+            byte[] bytes = new Byte[256];
+            stream.Read(bytes, 0, bytes.Length);
+            
+            return bytes;
+        }
+
+        public void send(byte[] s)
+        {
+            sendQueue = s;
+        }
+
+        public byte[] EncryptString(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
+                
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        public string DecryptString(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string. 
+                            plaintext = srDecrypt.ReadLine();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
